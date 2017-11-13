dbec30bcae8d9424e7ffb6b1bc347753c842386f
Matthew Wallis <matthewwallis@Matthews-MBP.home>
2017-11-13 07:13:19 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,15 +32,26 @@ var checkForMatch = function() {
 }
 }
 
-var flipCard = function(cardID) {
-console.log("You flipped " + cards[cardID].rank + ".");
+var flipCard = function() {
+	cardID = this.getAttribute("data-id");
+	console.log("You flipped " + cards[cardID].rank + ".");
 cardsInPlay.push(cards[cardID].rank);
 console.log(cards[cardID].suit);
 console.log(cards[cardID].cardImage);
+this.setAttribute('src', cards[cardID].cardImage);
 if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i <= cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
